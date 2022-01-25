a7372df529b7c0d0688b79767fd827da2e896463
Alice <alize_in_cryptoland@protonmail.com>
2022-01-25 15:04:43 -0500

    fix loop

--- routes/validate.js
+++ routes/validate.js
@@ -6,24 +6,27 @@ const connection = new Connection("https://alice.genesysgo.net");
 const sleep = (time = 1000) =>
   new Promise((resolve, reject) => setTimeout(() => resolve(undefined), time));
 
+async function getTxRecursively(txid) {
+  const tx = await connection.getConfirmedTransaction(txid);
+  if (tx) {
+    const msg1 = tx.meta.logMessages.find((m) => m.includes("log: Signed by"));
+    const msg2 = tx.meta.logMessages.find((m) => m.includes("log: Memo"));
+    return `${msg1}
+${msg2}`;
+  }
+
+  await sleep(1000);
+  return await getTxRecursively(txid);
+}
+
 router.get('/', async function (req, res, next) {
   const { txid } = req.query;
   if (!txid) {
     res.send({ error: 'no txid specified' });
     return
   }
-  let confirmed;
-  while (!confirmed) {
-    const tx = await connection.getConfirmedTransaction(txid);
-    if (tx) {
-      const msg1 = tx.meta.logMessages.find((m) => m.includes("log: Signed by"));
-      const msg2 = tx.meta.logMessages.find((m) => m.includes("log: Memo"));
-      logToDiscord(`${msg1} ${msg2}`);
-      confirmed = true;
-      res.send({ success: true })
-      return;
-    }
-    await sleep(1000);
-  }
+  const msg = await getTxRecursively(txid);
+  logToDiscord(msg);
+  res.send({ success: true });
 });
 module.exports = router;
