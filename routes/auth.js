53ebf2f7dae148355003c107145db9ef8f1025fd
Alice <alize_in_cryptoland@protonmail.com>
2022-02-25 07:46:26 -0500

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -55,10 +55,10 @@ router.get("/", async function (req, res, next) {
     headers: { Authorization: `Bearer ${resp.access_token}` },
   }).then((res) => res.json());
   const guild_id = process.env.GUILD_ID;
-  const me = client.guilds.cache.get(guild_id).members.cache.get(me.id);
+  const discordUser = client.guilds.cache.get(guild_id).members.cache.get(me.id);
   res.status(400).send({ error: "Not part of the server!" });
 
-  const hasRole = me.roles.cache.some((role) => role.name === "DELTA FORCE");
+  const hasRole = discordUser.roles.cache.some((role) => role.name === "DELTA FORCE");
   if (!hasRole) {
     res.status(400).send({ error: "`Error: Role not found`" });
     return;
